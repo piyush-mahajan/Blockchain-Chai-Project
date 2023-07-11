86892dc1604d779f5ee8046dd87d786967e88931
piyush-mahajan <98900169+piyush-mahajan@users.noreply.github.com>
2023-07-11 11:10:14 +0530

    problem for deployment on alchemy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
+//from alchemy deployment
+const GOERLI_URL = process.env.GOERLI_URL;
+//metamask account
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: GOERLI_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
